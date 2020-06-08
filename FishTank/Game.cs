af0a5ae46041992038331d47c26cda21a8e1dc88
Yuchen Li <ychnlgy.li@utoronto.ca>
2020-06-08 07:52:33 -0700

    Roughwork (#1)
    * Added initial base
    
    * Added basic entities
    
    * Added moving fish
    
    * Added sample image
    
    * Update README.md
    
    Co-authored-by: Yuchen Li <t-yucl@microsoft.com>

---
+++ FishTank/Game.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FishTank
+{
+    using FishTank.Source;
+
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        EntityManager manager;
+
+        Vector2 tankDim;
+
+        public Game()
+        {
+            tankDim = new Vector2(1280, 640);
+            graphics = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferWidth = (int) tankDim.X,
+                PreferredBackBufferHeight = (int) tankDim.Y
+            };
+            Content.RootDirectory = "Content";
+            manager = new EntityManager();
+            
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            ImageCreator backgroundCreator = new ImageCreator(1f, spriteBatch, Content);
+            ImageCreator characterCreator = new ImageCreator(0.5f, spriteBatch, Content);
+
+            manager.Add(new Entity(backgroundCreator.Create("resources/tank")));
+
+            Entity fish1 = new Fish(
+                characterCreator.Create("resources/fish1"), new Vector2(150, 200), false,
+                180, 80, tankDim);
+            Entity fish2 = new Fish(
+                characterCreator.Create("resources/fish2"), new Vector2(250, 300), false,
+                180, 80, tankDim);
+            Entity shrimp = new Entity(characterCreator.Create("resources/shrimp"), new Vector2(800, 500), true);
+            manager.Add(fish1);
+            manager.Add(fish2);
+            manager.Add(shrimp);
+
+            manager.Add(new Entity(backgroundCreator.Create("resources/plants")));
+            manager.Add(new Entity(backgroundCreator.Create("resources/rocks")));
+            manager.Add(new Entity(backgroundCreator.Create("resources/sand")));
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            manager.Update(dt);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            manager.Draw();
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ FishTank/Source/Entity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishTank.Source
+{
+    using Microsoft.Xna.Framework;
+
+    public class Entity
+    {
+        protected Image img;
+        protected Vector2 pos;
+        protected bool flip;
+        protected float rotation;
+
+        public Entity(Image img)
+        {
+            Init(img, new Vector2(0), false);
+        }
+
+        public Entity(Image img, Vector2 pos, bool flip)
+        {
+            Init(img, pos, flip);
+        }
+
+        public virtual void Update(float dt)
+        {
+
+        }
+
+        public void Draw()
+        {
+            img.Draw(pos, flip, rotation);
+        }
+
+        private void Init(Image img, Vector2 pos, bool flip)
+        {
+            this.img = img;
+            this.pos = pos;
+            this.flip = flip;
+        }
+    }
+}
